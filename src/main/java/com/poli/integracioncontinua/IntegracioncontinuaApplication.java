b50f38ce6094a819fa07a857af8b0d6d18d32a12
dsevillanocardona <dsevillanocardona@twilio.com>
2023-10-31 19:06:07 -0500

    Baseline

---
+++ src/main/java/com/poli/integracioncontinua/IntegracioncontinuaApplication.java
@@ -0,0 +1,13 @@
+package com.poli.integracioncontinua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracioncontinuaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegracioncontinuaApplication.class, args);
+    }
+
+}
